56639b81eb38d258ae3434ec341df25c4337fe03
Brandon Ros <brandonros1@gmail.com>
2025-02-27 13:41:30 -0500

    regex

--- src/utils/tableOperations.ts
+++ src/utils/tableOperations.ts
@@ -1,11 +1,23 @@
 import { Filters, Sorts, Rows } from '../types';
 
 export const applyFilters = (rows: Rows, filters: Filters) => {
+    console.log({
+        filters
+    })
     return rows.filter(row => {
         return Object.entries(filters).every(([key, value]) => {
             // Skip filtering if the value is an empty string
             if (value === '') return true;
-            return row[key as keyof typeof row] === value;
+
+            // Convert the filter value to a RegExp object
+            try {
+                const regex = new RegExp(value);
+                const fieldValue = String(row[key as keyof typeof row]);
+                return regex.test(fieldValue);
+            } catch (error) {
+                console.warn(`Invalid regex pattern for ${key}: ${value}`, error);
+                return false;
+            }
         });
     });
 };
