9c0502dff9f6700d50114a29df4eddcdcd8a037c
Brandon Ros <brandonros1@gmail.com>
2025-03-05 22:57:49 -0500

    75

--- src/utils/tableUtils.ts
+++ src/utils/tableUtils.ts
@@ -10,19 +10,19 @@ export const getSortIndicator = (sorts: Sorts, columnName: string) => {
 
 export const calculateColumnWidth = (column: Column) => {
     if (column.name === 'delta') {
-        return 70;
+        return 75;
     }
     if (column.name === 'gamma') {
-        return 70;
+        return 75;
     }
     if (column.name === 'theta') {
-        return 70;
+        return 75;
     }
     if (column.name === 'vega') {
-        return 70;
+        return 75;
     }
     if (column.name === 'rho') {
-        return 70;
+        return 75;
     }
     return column.name.length * 10;
 }
