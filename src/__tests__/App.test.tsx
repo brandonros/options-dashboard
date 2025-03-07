ecbdcadd9888f7c73cd5b5d884572aceed2bafdd
Brandon Ros <brandonros1@gmail.com>
2025-03-06 20:03:44 -0500

    tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { TableProvider, useTableContext } from '../providers/TableProvider';
+import { describe, it, expect } from '@jest/globals'
+
+describe('DataTable', () => {
+  it('renders without crashing', () => {
+    expect(true).toBe(true);
+  });
+});
