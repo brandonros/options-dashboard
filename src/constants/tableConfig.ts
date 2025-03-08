abd09cf78bd0a681ca7e8d645bdb4aea00009fd7
Brandon Ros <brandonros1@gmail.com>
2025-03-07 20:44:04 -0500

    coulmns

--- src/constants/tableConfig.ts
+++ src/constants/tableConfig.ts
@@ -15,18 +15,18 @@ export const COLUMNS =[
     { name: 'capital_commitment_period', type: 'number', alias: 'num_days' },
 
     { name: 'simple_roi', type: 'percentage', alias: 'roi' },
-    { name: 'daily_simple_roi', type: 'percentage', alias: 'daily_roi' },
-    { name: 'annualized_simple_roi', type: 'percentage', alias: 'yearly_roi' },
+    { name: 'daily_simple_roi', type: 'percentage', alias: 'roi_day' },
+    { name: 'annualized_simple_roi', type: 'percentage', alias: 'roi_year' },
 
-    { name: 'risk_adjusted_daily_simple_roi', type: 'percentage', alias: 'adj_daily_roi' },
+    { name: 'risk_adjusted_daily_simple_roi', type: 'percentage', alias: 'adj_roi_day' },
     { name: 'implied_volatility', type: 'percentage', alias: 'iv' },
     { name: 'volatility_prob_itm_ratio', type: 'percentage', alias: 'vol_prob_itm_ratio' },
 
-    { name: 'implied_move_percent', type: 'percentage', alias: 'implied_move_pct' },
-    { name: 'implied_move_dollars', type: 'currency', alias: 'implied_move_usd' },
-    { name: 'margin_of_safety', type: 'currency', alias: 'margin_of_safety_usd' },
-    { name: 'margin_of_safety_percentage', type: 'percentage', alias: 'margin_of_safety_pct' },
-    { name: 'adjusted_margin_of_safety_percentage', type: 'percentage', alias: 'adj_margin_of_safety_pct' },
+    { name: 'implied_move_percent', type: 'percentage', alias: 'impl_move_pct' },
+    { name: 'implied_move_dollars', type: 'currency', alias: 'impl_move_usd' },
+    { name: 'margin_of_safety', type: 'currency', alias: 'safety_margin_usd' },
+    { name: 'margin_of_safety_percentage', type: 'percentage', alias: 'safety_margin_pct' },
+    { name: 'adjusted_margin_of_safety_percentage', type: 'percentage', alias: 'adj_safety_margin_pct' },
 
     // { name: 'daily_secured_ror_per_iv', type: 'percentage' },
     // { name: 'daily_secured_ror_per_implied_move', type: 'percentage' },
@@ -39,7 +39,7 @@ export const COLUMNS =[
     { name: 'ask_price', type: 'currency', alias: 'ask' },
     { name: 'last_trade_price', type: 'currency', alias: 'last' },
     { name: 'bid_ask_spread', type: 'currency', alias: 'spread' },
-    { name: 'slippage_impact', type: 'percentage', alias: 'slippage_impact_pct' },
+    { name: 'slippage_impact', type: 'percentage', alias: 'slippage_pct' },
 
     { name: 'volume', type: 'number', alias: 'volume' },
     { name: 'open_interest', type: 'number', alias: 'oi' },
@@ -49,7 +49,7 @@ export const COLUMNS =[
     { name: 'extrinsic_value', type: 'currency', alias: 'ext_value' },
 
     { name: 'break_even', type: 'currency' },
-    { name: 'distance_to_breakeven', type: 'currency' },
+    { name: 'distance_to_breakeven', type: 'currency', alias: 'dist_to_breakeven' },
 
     { name: 'moneyness_ratio', type: 'percentage' },
 
